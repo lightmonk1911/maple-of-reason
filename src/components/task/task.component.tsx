75ef85da7d7e481209ac36e4ebafcf907f482696
ibabenkov <lightmonk1911@gmail.com>
2019-09-30 00:08:28 +0400

    [feat] task component

---
+++ src/components/task/task.component.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { ITaskProps } from './task.types';
+import block from '../../../node_modules/bem-cn/lib/index';
+
+const b = block('task');
+export const Task: React.FunctionComponent<ITaskProps> = props => {
+  return (
+    <div className={b() + ` ${props.className}`}>Task component works!</div>
+  );
+};
---
+++ src/components/task/task.test.tsx
@@ -0,0 +1,29 @@
+import { act } from 'react-dom/test-utils';
+import ReactDOM from 'react-dom';
+import React from 'react';
+import { Task } from './task.component';
+
+describe('should render with bem', (): void => {
+  let container: HTMLElement | null;
+  beforeEach((): void => {
+    container = document.createElement('div');
+    document.body.appendChild(container);
+
+    act((): void => {
+      ReactDOM.render(<Task className="mix" />, container);
+    });
+  });
+
+  afterEach((): void => {
+    if (container) {
+      document.body.removeChild(container);
+    }
+    container = null;
+  });
+
+  test('should have mixed class', () => {
+    const element = container && container.querySelector('*');
+    console.log('element', element && element.classList);
+    expect(element).toBeInstanceOf(Element);
+  });
+});
