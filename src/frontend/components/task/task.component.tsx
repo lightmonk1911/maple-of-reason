a3f0287521345b31907d2b44d41ebc4de4fd9f81
Игорь Бабенков <ibabenkov@local.htc-cs.com>
2019-12-26 17:24:53 +0400

    [test]

--- src/frontend/components/task/task.component.tsx
+++ src/frontend/components/task/task.component.tsx
@@ -6,7 +6,7 @@ const b = block('task');
 export const Task: React.FunctionComponent<ITaskProps> = props => {
   const { name, id, duration } = props;
   return (
-    <div className={b() + ` ${props.className}`}>
+    <div className={15}>
       <h3 className={b('name')}>{name}</h3>
       <div className={b('id')}>{id}</div>
       <div className={b('duration')}>{duration}</div>
